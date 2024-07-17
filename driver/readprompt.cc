78fb2b6f33a5f2a549bc09e998c8050a607c7aab
mrieker@nii.net <mrieker@nii.net>
2024-07-16 20:02:50 -0400

    driver: fix readprompt to work even when stdout is teed

--- driver/readprompt.cc
+++ driver/readprompt.cc
@@ -22,6 +22,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include <readline/history.h>
 #include <readline/readline.h>
@@ -30,11 +31,39 @@
 
 char const *readprompt (char const *prompt)
 {
-    char *expansion, *line;
-    int result;
-
     static char *lastline = NULL;
     static int initted = 0;
+
+    // if stdin not a tty, just read next line from stdin and echo to stdout
+    if (!isatty (STDIN_FILENO)) {
+        if (lastline != NULL) free (lastline);
+        int linelen = 256;
+        int lineofs = -1;
+        lastline = (char *) malloc (linelen);
+        do {
+            if (linelen - ++ lineofs < 2) {
+                linelen += linelen / 2;
+                lastline = (char *) realloc (lastline, linelen);
+            }
+            if (lastline == NULL) abort ();
+            int ll;
+            if ((fgets (lastline + lineofs, linelen - lineofs, stdin) == NULL) || ((ll = strlen (lastline + lineofs)) <= 0)) {
+                if (lineofs > 0) break;
+                free (lastline);
+                lastline = NULL;
+                printf ("%s*EOF*\n", prompt);
+                return NULL;
+            }
+            lineofs += ll;
+        } while (lastline[--lineofs] != '\n');
+        lastline[lineofs] = 0;
+        printf ("%s%s\n", prompt, lastline);
+        return lastline;
+    }
+
+    // stdin is a tty, use libreadline
+    char *expansion, *line;
+    int result;
 
     if (!initted) {
         using_history ();
@@ -42,15 +71,32 @@ char const *readprompt (char const *prompt)
         history_expansion_char = 0;
         rl_bind_key ('\t', rl_insert);
         initted = 1;
+
+        // if stdout is not a tty, use stdin's tty for prompting and echo to stdout
+        if (!isatty (STDOUT_FILENO)) {
+            char stdinfn[20];
+            snprintf (stdinfn, sizeof stdinfn, "/proc/self/fd/%d", STDIN_FILENO);
+            rl_outstream = fopen (stdinfn, "w");
+            if (rl_outstream == NULL) {
+                fprintf (stderr, "readprompt: error opening %s: %m\n", stdinfn);
+            } else {
+                initted = -1;
+            }
+        }
     }
 
 readit:
     line = readline (prompt);
-    if (line == NULL) return NULL;
+    FILE *echoto = (rl_outstream == NULL) ? stdout : rl_outstream;
+    if (line == NULL) {
+        fputs ("*EOF*\n", echoto);
+        if (initted < 0) printf ("%s*EOF*\n", prompt);
+        return NULL;
+    }
     if ((line[0] != 0) && ((lastline == NULL) || (strcmp (line, lastline) != 0))) {
         result = history_expand (line, &expansion);
         if (result != 0) {
-            fprintf (stderr, "%s\n", expansion);
+            fprintf (echoto, "%s\n", expansion);
         }
         if ((result < 0) || (result == 2)) {
             free (expansion);
@@ -62,5 +108,6 @@ readit:
     }
     if (lastline != NULL) free (lastline);
     lastline = line;
+    if (initted < 0) printf ("%s%s\n", prompt, lastline);
     return lastline;
 }
